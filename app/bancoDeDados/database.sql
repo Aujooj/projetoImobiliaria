d6864da6042979e8d369bea53a64e8fac305603f
João Augusto de Souza Santos <jhojhoaugusto@gmail.com>
2023-10-31 16:27:28 -0300

    Novos corretores no script

--- app/bancoDeDados/database.sql
+++ app/bancoDeDados/database.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE Estelar;
+Use Estelar;
 -- ---------------------------------------------------------------------------------------------------------
 -- Criação das tabelas
 
 CREATE TABLE corretores (
   id_corretor INT AUTO_INCREMENT PRIMARY KEY,
-  creci VARCHAR(8) NOT NULL,
+  creci VARCHAR(5) NOT NULL,
   senha VARCHAR(60) NOT NULL,
   data_nasc DATE NOT NULL,
   nome VARCHAR(100) NOT NULL,
@@ -60,5 +61,10 @@ CREATE TABLE contratos (
 
 INSERT INTO corretores (creci, senha, data_nasc, nome, telefone, email) 
 VALUES 
-('12345678', '$2y$13$7OLpVlvrM.TZyZ7ccnu3N.03ZE2teOeC/QKDKlpTu6UDPddCMmxTW', '2002-03-20', 'João Augusto de Souza Santos', '1228611876', 'jsantos.2021@alunos.utfpr.edu.br'), 
-('87654321', '$2y$13$wtz3m3cNlDLNOyNQgylM0uSTw1TkE7PNZPAOfqOEz2tKoyij1Tj6O', '1996-11-17', 'Wagner Cordeiro Gomes da Silva', '42980224425', 'wagnercordeiro@alunos.utfpr.edu.br');
+('J0123', '$2y$13$J0/b4V8tVzzdC.UWiZwx4OpWSVc8r4ylIPiA.v3oxdqKtZmcKNM6K', '2003-04-12', 'Gabriel Assunção', '4122648187', 'gabrielbrunhara@alunos.utfpr.edu.br'), 
+
+('J4567', '$2y$13$7OLpVlvrM.TZyZ7ccnu3N.03ZE2teOeC/QKDKlpTu6UDPddCMmxTW', '2002-03-20', 'João Augusto', '1228611876', 'jsantos.2021@alunos.utfpr.edu.br'),
+
+('J8910', '$2y$13$QPgBc6cFl9bY6bqPOTzcP.77.qVqcrXQeCI7RqsS.CyqYjwyCoMhC', '2002-03-23', 'Luis Nathan', '42928231876', 'roncolato@alunos.utfpr.edu.br'), 
+
+('J1112', '$2y$13$wtz3m3cNlDLNOyNQgylM0uSTw1TkE7PNZPAOfqOEz2tKoyij1Tj6O', '1996-11-17', 'Wagner Cordeiro', '42980224425', 'wagnercordeiro@alunos.utfpr.edu.br');
