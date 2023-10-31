4137e3de3a30e42ade7b33cb86b9656fa53d2429
João Augusto de Souza Santos <jhojhoaugusto@gmail.com>
2023-10-31 16:39:27 -0300

    Corrigindo erros do script

--- app/bancoDeDados/database.sql
+++ app/bancoDeDados/database.sql
@@ -13,7 +13,7 @@ CREATE TABLE corretores (
   email VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE clientes(
+CREATE TABLE clientes (
   id_cliente INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
   cpf VARCHAR(14) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE clientes(
 );
 
 CREATE TABLE imoveis (
-  id_produto INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id_imovel INTEGER AUTO_INCREMENT PRIMARY KEY,
   tipo ENUM('Apartamento', 'Casa Residencial', 'Chácara', 'Comercial', 'Condomínio', 'Terreno'),
   cep INTEGER NOT NULL,
   rua VARCHAR(100),
@@ -43,8 +43,8 @@ CREATE TABLE imoveis (
   dormitorios INT,
   banheiros INT,
   garagem INT,
-  id_contrato INT NOT NULL FOREIGN KEY REFERENCES contratos(id_contrato),
-  proprietario INT NOT NULL FOREIGN KEY REFERENCES clientes(id_cliente)
+  proprietario INT NOT NULL,
+  FOREIGN KEY (proprietario) REFERENCES clientes(id_cliente)
 );
 
 CREATE TABLE contratos (
@@ -52,19 +52,17 @@ CREATE TABLE contratos (
   tipo ENUM('Locação', 'Venda'),
   data_inicial DATETIME,
   data_final DATETIME,
-  inquilino INT NOT NULL FOREIGN KEY REFERENCES clientes(id_cliente),
-  id_imovel INT NOT NULL FOREIGN KEY REFERENCES imovel(id_imovel)
+  inquilino INT NOT NULL,
+  id_imovel INT NOT NULL,
+  FOREIGN KEY (inquilino) REFERENCES clientes(id_cliente),
+  FOREIGN KEY (id_imovel) REFERENCES imoveis(id_imovel)
 );
 
 -- ---------------------------------------------------------------------------------------------------------
 -- Inserção de dados
 
-INSERT INTO corretores (creci, senha, data_nasc, nome, telefone, email) 
-VALUES 
-('J0123', '$2y$13$J0/b4V8tVzzdC.UWiZwx4OpWSVc8r4ylIPiA.v3oxdqKtZmcKNM6K', '2003-04-12', 'Gabriel Assunção', '4122648187', 'gabrielbrunhara@alunos.utfpr.edu.br'), 
-
+INSERT INTO corretores (creci, senha, data_nasc, nome, telefone, email) VALUES 
+('J0123', '$2y$13$J0/b4V8tVzzdC.UWiZwx4OpWSVc8r4ylIPiA.v3oxdqKtZmcKNM6K', '2003-04-12', 'Gabriel Assunção', '4122648187', 'gabrielbrunhara@alunos.utfpr.edu.br'),
 ('J4567', '$2y$13$7OLpVlvrM.TZyZ7ccnu3N.03ZE2teOeC/QKDKlpTu6UDPddCMmxTW', '2002-03-20', 'João Augusto', '1228611876', 'jsantos.2021@alunos.utfpr.edu.br'),
-
-('J8910', '$2y$13$QPgBc6cFl9bY6bqPOTzcP.77.qVqcrXQeCI7RqsS.CyqYjwyCoMhC', '2002-03-23', 'Luis Nathan', '42928231876', 'roncolato@alunos.utfpr.edu.br'), 
-
+('J8910', '$2y$13$QPgBc6cFl9bY6bqPOTzcP.77.qVqcrXQeCI7RqsS.CyqYjwyCoMhC', '2002-03-23', 'Luis Nathan', '42928231876', 'roncolato@alunos.utfpr.edu.br'),
 ('J1112', '$2y$13$wtz3m3cNlDLNOyNQgylM0uSTw1TkE7PNZPAOfqOEz2tKoyij1Tj6O', '1996-11-17', 'Wagner Cordeiro', '42980224425', 'wagnercordeiro@alunos.utfpr.edu.br');
